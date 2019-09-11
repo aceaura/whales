8a7662729b7241c66c7f6bb339cfc06a422f1902
lizongti <44680454+lizongti@users.noreply.github.com>
2019-09-11 17:42:09 +0800

    add print

--- script/mysql-5-spark-dump-entrypoint.py
+++ script/mysql-5-spark-dump-entrypoint.py
@@ -1,18 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
 import os, sys, time, importlib, multiprocessing
-sys.path.append("/" if "WORK_DIR" not in os.environ else os.environ["WORK_DIR"])
+
+work_dir = "/" if "WORK_DIR" not in os.environ else os.environ["WORK_DIR"]
+sys.path.append(work_dir)
+print("Work directory is set as {}".format(work_dir))
 
 mysql_host = "localhost" if "MYSQL_HOST" not in os.environ else os.environ["MYSQL_HOST"]
 mysql_port = 3306 if "MYSQL_PORT" not in os.environ else int(os.environ["MYSQL_PORT"])
 mysql_user = "root" if "MYSQL_USER" not in os.environ else os.environ["MYSQL_USER"]
 mysql_password = "root" if "MYSQL_PASSWORD" not in os.environ else os.environ["MYSQL_PASSWORD"]
 url = "jdbc:mysql://{}:{}?user={}&password={}".format(mysql_host, mysql_port, mysql_user, mysql_password)
+print("JDBC url is set as {}".format(url))
+
 table = sys.argv[1]
+print("Table name is set as {}", table)
+
 properties={"driver": "com.mysql.jdbc.Driver"}
-cpu_count = multiprocessing.cpu_count()
 map = importlib.import_module(sys.argv[2]).map
 output_dir = "{}/{}/{}".format("/" if "OUTPUT_DIR" not in os.environ else os.environ["OUTPUT_DIR"], table, time.time()) 
+print("Output Directory is set as {}".format(output_dir))
+
+cpu_count = multiprocessing.cpu_count()
+print("Repatitions is set as {}".format(cpu_count))
 
 spark = SparkSession.builder.config(conf = SparkConf()).getOrCreate()
 spark.read.jdbc(
