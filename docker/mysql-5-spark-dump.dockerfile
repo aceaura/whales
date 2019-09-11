19637ab086b04b01a955b18492476fcc978403a3
lizongti <44680454+lizongti@users.noreply.github.com>
2019-09-11 16:24:58 +0800

    add mysql-5-spark-dump dockerfile

---
+++ docker/mysql-5-spark-dump.dockerfile
@@ -0,0 +1,13 @@
+FROM bde2020/spark-base:2.4.1-hadoop2.7
+
+ENV MYSQL_HOST=0.0.0.0 \
+    MYSQL_PORT=3306 \
+    MYSQL_USER=root \
+    MYSQL_PASSWORD=root \
+    OUTPUT_DIR=/mysql-dump
+
+COPY /script/mysql-5-spark-dump-entrypoint.py /
+
+WORKDIR /mysql-dump
+
+ENTRYPOINT ["/spark/bin/spark-submit", "--packages=mysql:mysql-connector-java:5.1.48", "/mysql-5-spark-dump-entrypoint.py"]
---
+++ script/mysql-5-spark-dump-entrypoint.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+import os, sys, time, importlib, multiprocessing
+
+mysql_host = "localhost" if "MYSQL_HOST" not in os.environ else os.environ["MYSQL_HOST"]
+mysql_port = 3306 if "MYSQL_PORT" not in os.environ else int(os.environ["MYSQL_PORT"])
+mysql_user = "root" if "MYSQL_USER" not in os.environ else os.environ["MYSQL_USER"]
+mysql_password = "root" if "MYSQL_PASSWORD" not in os.environ else os.environ["MYSQL_PASSWORD"]
+url = "jdbc:mysql://{}:{}?user={}&password={}".format(mysql_host, mysql_port, mysql_user, mysql_password)
+table = sys.argv[1]
+properties={"driver": "com.mysql.jdbc.Driver"}
+cpu_count = multiprocessing.cpu_count()
+map = importlib.import_module(sys.argv[2]).map
+output_dir = "{}/{}/{}".format("/" if "OUTPUT_DIR" not in os.environ else os.environ["OUTPUT_DIR"], table, time.time()) 
+
+spark = SparkSession.builder.config(conf = SparkConf()).getOrCreate()
+spark.read.jdbc(
+    url = url, table = table, properties = properties
+).rdd.repartition(cpu_count).map(map).repartition(1).saveAsTextFile(output_dir)
+spark.stop()
