12d6ae91cb926b7a488741b481a03ca07c8c5882
lizongti <44680454+lizongti@users.noreply.github.com>
2019-09-17 23:18:59 +0800

    fix repartition

--- script/mysql-5-spark-dump-entrypoint.py
+++ script/mysql-5-spark-dump-entrypoint.py
@@ -7,14 +7,14 @@ mysql_user = "root" if "MYSQL_USER" not in os.environ else os.environ["MYSQL_USE
 mysql_password = "root" if "MYSQL_PASSWORD" not in os.environ else os.environ["MYSQL_PASSWORD"]
 jdbc_url = "jdbc:mysql://{}:{}?user={}&password={}".format(mysql_host, mysql_port, mysql_user, mysql_password)
 work_dir = "/mysql-dump" if "WORK_DIR" not in os.environ else os.environ["WORK_DIR"]
-output_dir = "{}/{}/{}".format("/mysql-dump" if "OUTPUT_DIR" not in os.environ else os.environ["OUTPUT_DIR"], sys.argv[1], time.time()) 
-cpu_count = multiprocessing.cpu_count()
+output_dir = "{}/{}/{}".format("/mysql-dump" if "OUTPUT_DIR" not in os.environ else os.environ["OUTPUT_DIR"], sys.argv[1], time.time())
+repartition_count = multiprocessing.cpu_count() if "REPARTITION_COUNT" not in os.environ else int(os.environ["REPARTITION_COUNT"])
 table_name = sys.argv[1]
 
 commands = [
     "mkdir /mysql-dump-run/ && cp -rf /script/mysql-5-spark-dump-runner.py /mysql-dump-run/ && cp -rf {}/* /mysql-dump-run/".format(work_dir),
     "mv /mysql-dump-run/{}.py /mysql-dump-run/mysql_5_dump_mapper.py".format(sys.argv[2]),
-    "cd /mysql-dump-run/ && /spark/bin/spark-submit --packages=mysql:mysql-connector-java:5.1.48 mysql-5-spark-dump-runner.py '{}' '{}' {} '{}'".format(jdbc_url, table_name, cpu_count, output_dir)
+    "cd /mysql-dump-run/ && /spark/bin/spark-submit --packages=mysql:mysql-connector-java:5.1.48 mysql-5-spark-dump-runner.py '{}' '{}' {} '{}'".format(jdbc_url, table_name, repartition_count, output_dir)
 ]
 for command in commands:
     print("OS Execute: {}".format(command))
