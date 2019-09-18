d38af452c0f8dd43e66a972705bb2fb52379769f
lizongti <44680454+lizongti@users.noreply.github.com>
2019-09-18 10:23:00 +0800

    fix mysql-5-spark-dump

--- script/mysql-5-spark-dump-runner.py
+++ script/mysql-5-spark-dump-runner.py
@@ -7,5 +7,5 @@ spark = SparkSession.builder.config(conf = SparkConf()).getOrCreate()
 from mysql_5_dump_mapper import main as map
 spark.read.jdbc(
     url = sys.argv[1], table = sys.argv[2], properties = {"driver": "com.mysql.jdbc.Driver"}
-).rdd.repartition(int(sys.argv[3])).map(map).repartition(1).saveAsTextFile(sys.argv[4])
+).rdd.repartition(int(sys.argv[3])).map(map).repartition(int(sys.argv[4])).saveAsTextFile(sys.argv[5])
 spark.stop()
