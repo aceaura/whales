35f27568ec4b3b0c78c333f257f525e559fae581
lizongti <lzongti@gmail.com>
2022-09-29 12:34:37 +0800

    Add template render

---
+++ template/render.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf ./github/workflows/* && \
+find ./docker -name "*.Dockerfile" | \
+awk -F '[/|.]' '{print $4}'| \
+xargs -I {} \
+env "$(echo 'tag={}')" \
+env "$(echo 'username=${{ secrets.DOCKERHUB_USERNAME }}')" \
+env "$(echo 'password=${{ secrets.DOCKERHUB_TOKEN }}')" \
+gomplate -f ./template/workflow.template.yml -o ./.github/workflows/{}.yml
+
